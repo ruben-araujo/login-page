e48d0fd91198d4808c64c073c66b58c3e79f2d25
ruben-araujo <rubenaraujo097@gmail.com>
2026-02-16 14:15:35 -0300

    adicionando funcionalidade ao botão checkbox

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function logar () {
     const usuario = document.getElementById('usuario').value;
     const senha = document.getElementById('senha').value;
     const mensagem = document.getElementById('mensagem');
+    const checkbox = document.getElementById('lembrar');
 
     const usuarioCorreto = "laranja123";
     const senhaCorreta = "salada123";
@@ -17,4 +18,20 @@ function logar () {
         mensagem.style.color = "red";
         mensagem.innerText = "Usuário ou Senha incorretos";
     }
+
+
+    if (checkbox.checked) {
+        localStorage.setItem('usuarioSalvo', usuario);
+    } else {
+        localStorage.removeItem('usuarioSalvo');
+    }
 }
+
+window.onload = function() {
+    const nomeSalvo = localStorage.getItem('usuarioSalvo');
+    if(nomeSalvo) {
+        document.getElementById('usuario').value = nomeSalvo;
+        document.getElementById('lembrar').checked = true;
+    }
+};
+
